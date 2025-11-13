03e52a71e8a6f8de956ae8af3c0d8b2bb803a583
Gogo602 <gogomarosi61@gmail.com>
2025-11-13 09:05:17 +0300

    Display prodcut list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-
-
-import React from 'react'
+import {useEffect, useState } from 'react'
+import ProductList from './components/ProductList';
 
 export default function App() {
+  const [products, setProducts] = useState(null);
+  const [ error, setError ] = useState(null)
+  const [ loading, setLoading ] = useState(true)
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try{
+          const res = await fetch("http://localhost:8000/products")
+          const data = await res.json()
+          setProducts(data)
+      }catch(err){
+        setError(err.message)
+      }finally{
+        setLoading(false)
+      }
+    }
+    getProducts()
+  },[])
+
+  console.log(products)
+
+
+      
+
   return (
-    <div className="text-lg p-5">App</div>
+    <div className="flex items-center justify-center p-6 min-h-screen w-full bg-gray-700">
+      <div className='space-y-8'>
+        <h2 className='text-3xl font-bold text-white'>Product Catalogue</h2>
+        {loading && (
+          <p>Loading...</p>
+        )}
+        {error && (
+          <p>Loading...</p>
+        )}
+        <ProductList products={products}/>
+      </div>
+    </div>
   )
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+export default function ProductCard({product}) {
+  return (
+   <div className='border p-6 rounded-md bg-white space-y-3'>
+        <img src={product.image} alt={product.name} className='h-40 object-cover mx-auto'/>
+        <h2 className='text-xl font-bold'>{product.name}</h2>
+        <p>{product.description}</p>
+        <p>Price: {" "}${product.price}</p>
+        <p>Quantity: {" "}{product.quantity}</p>
+        <div className='flex items-center justify-between'>
+        <p>Category: {" "}{product.category}</p>
+        <p>Ratings: {" "}{product.rating}</p>
+        </div>
+    </div>
+  )
+}
